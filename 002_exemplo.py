8d5cdcff657c05897a55b11f25458a59f323bc07
Orlando Santana <santanaaorlando@gmail.com>
2024-07-03 22:39:00 -0400

    Adicionando função len ao input do dado

--- 002_exemplo.py
+++ 002_exemplo.py
@@ -1,7 +1,10 @@
 
+#Crie um programa que o usuário digite um nome e retorne o nome e também o nº de letras que tem o nome
 
 question = "Qual o melhor jogador que viu jogar? "
 awnser = input(question)
 return_ = "Melhor jogador "
+len_awnser = len(awnser)
 
 print(return_ + awnser)
+print("Nº de letras: " + str(len_awnser))
